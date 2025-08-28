880231d66b9af25a0fd8d04c58e74ac2eff764ed
niraj2024407 <neerajkumar04051998@gmail.com>
2025-08-28 07:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function getFormvalue() {
     //Write your code here
+	// Get values and trim extra spaces
+      let firstName = document.getElementById("fname").value.trim();
+      let lastName = document.getElementById("lname").value.trim();
+
+      // Combine full name
+      let fullName = firstName + (lastName ? " " + lastName : "");
+
+      if (fullName) {
+        alert(fullName);
+      } else {
+        alert("Please enter your name.");
+      }
+
+      // Prevent default form submission
+      return false;
 
 }
